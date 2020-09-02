c6b3c99fa120d05e13d3516684a1f37a0df11a51
jdiazcano <terkitos@gmail.com>
2020-09-02 10:27:47 +0200

    Impl syncws

--- ocean/src/commands/syncws.rs
+++ ocean/src/commands/syncws.rs
@@ -1,6 +1,8 @@
 use crate::commands::{ExecutableCommand, SyncwsCommand};
 use clap::{ArgMatches, App};
 use build_system::beans::UserConfiguration;
+use std::path::Path;
+use std::process::Command;
 
 impl SyncwsCommand {
     #[inline]
@@ -11,6 +13,24 @@ impl SyncwsCommand {
 
 impl ExecutableCommand for SyncwsCommand {
     fn handle(&self, config: UserConfiguration, matches: &ArgMatches) {
+        let root = option_env!("WORKSPACE_ROOT").expect("Workspace root not set.");
+        let root = Path::new(root);
 
+        for project in config.projects {
+            println!("Checking {}", project.folder);
+            let project_folder = root.join(project.folder);
+            for repo in project.repositories {
+                let repo_folder = project_folder.join(&repo.folder);
+                if !repo_folder.exists() {
+                    let mut command = Command::new("git");
+                    command.arg("-C");
+                    command.arg(repo_folder.to_str());
+                    command.arg("clone");
+                    command.arg("--recurse-submodules");
+                    command.arg(repo.url);
+                    command.arg(&repo.folder);
+                }
+            }
+        }
     }
 }
